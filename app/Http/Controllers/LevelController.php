134aa3805d678ad3df6f5275db8ac2bfcb7cedf9
Patrick <patrickpierre2015@gmail.com>
2024-09-06 18:29:14 -0300

    feito paginação e crud completo

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Level;
+use Exception;
 use Illuminate\Http\Request;
 
 class LevelController extends Controller
@@ -11,10 +12,12 @@ class LevelController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $levels = Level::all();
-
+        $perPage = $request->get('per_page', 10);
+    
+        $levels = Level::paginate($perPage);
+    
         return response()->json($levels);
     }
 
@@ -31,18 +34,26 @@ class LevelController extends Controller
      */
     public function store(Request $request)
     {
+        // Validação com regras e mensagens personalizadas
         $validatedData = $request->validate([
-            'nivel' => 'required|string|max:255',
+            'nivel' => 'required|string|max:100',
+        ], [
+            'nivel.required' => 'O campo nível é obrigatório.',
+            'nivel.string' => 'O campo nível deve ser uma string.',
+            'nivel.max' => 'O campo nível não pode ter mais de 100 caracteres.',
         ]);
 
-        $level = Level::create([
-            'nivel' => $validatedData['nivel'],
-        ]);
+        try {
+            $level = Level::create($validatedData);
 
-        return response()->json([
-            'message' => 'Level criado com sucesso!',
-            'level' => $level,
-        ], 201);
+            return response()->json($level, 201);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao tentar cadastrar o nível. Por favor, tente novamente.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -66,7 +77,38 @@ class LevelController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        {
+            $validatedData = $request->validate([
+                'nivel' => 'required|string|max:255',
+            ], [
+                'nivel.required' => 'O campo nível é obrigatório.',
+                'nivel.string' => 'O campo nível deve ser uma string válida.',
+                'nivel.max' => 'O campo nível não pode ter mais que 255 caracteres.',
+            ]);
+    
+            $level = Level::find($id);
+    
+            if (!$level) {
+                return response()->json([
+                    'message' => 'Nível não encontrado.',
+                ], 404);
+            }
+    
+            try {
+                $level->update([
+                    'nivel' => $validatedData['nivel'],
+                ]);
+    
+                return response()->json([
+                    'message' => 'Nível atualizado com sucesso!',
+                    'data' => $level,
+                ], 200);
+            } catch (Exception $e) {
+                return response()->json([
+                    'message' => 'Erro ao tentar atualizar o nível.',
+                ], 500);
+            }
+        }
     }
 
     /**
@@ -74,6 +116,25 @@ class LevelController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $level = Level::find($id);
+
+        if (!$level) {
+            return response()->json([
+                'message' => 'Nível não encontrado.',
+            ], 404); 
+        }
+
+        try {
+            $level->delete();
+
+            return response()->json([
+                'message' => 'Nível deletado com sucesso!',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Erro ao tentar deletar o nível.',
+                'error'=> $e->getMessage(),
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,18 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [AuthController::class, 'login']);
+//Route::middleware('auth:sanctum')->post('/levels', [LevelController::class, 'store']);
+
+// Rotas para Levels 
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class,'store']);
+Route::put('/levels/{id}', [LevelController::class,'update']);
+Route::delete('/levels/{id}', [LevelController::class,'destroy']);
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/user', [UserController::class, 'index']);
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/levels', [LevelController::class, 'store']);
+
+
+
